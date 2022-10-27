7e436e5eb2834c7fddb2d7e4b091eba8893b2a74
BhanukaAGH <it20009472@my.sliit.lk>
2022-10-27 15:08:32 +0530

    add aspect ratio to image

--- lib/widgets/my fundraises/my_fundraise_card.dart
+++ lib/widgets/my fundraises/my_fundraise_card.dart
@@ -55,152 +55,157 @@ class MyFundraiseCard extends StatelessWidget {
             ),
             child: LayoutBuilder(
               builder: (BuildContext context, BoxConstraints constraints) =>
-                  Row(
-                children: [
-                  Stack(
-                    alignment: Alignment.topRight,
-                    children: [
-                      Image.network(
-                        imageUrl,
-                        width: constraints.maxWidth * 0.36,
-                        height: 118,
-                        fit: BoxFit.cover,
-                      ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Container(
-                          margin: const EdgeInsets.only(right: 6, top: 6),
-                          padding: const EdgeInsets.all(8),
-                          decoration: BoxDecoration(
-                            color: primaryColor,
-                            border: Border.all(
-                              width: 0.8,
-                              color: borderColor,
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: const Icon(
-                            Icons.bookmark_outline,
-                            size: 16,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    width: constraints.maxWidth * 0.62,
-                    height: 118,
-                    padding:
-                        const EdgeInsets.only(top: 14, left: 8, bottom: 14),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  IntrinsicHeight(
+                child: Row(
+                  children: [
+                    Stack(
+                      alignment: Alignment.topRight,
                       children: [
-                        Text(
-                          title,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                          textAlign: TextAlign.left,
-                          style: GoogleFonts.urbanist(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w700,
+                        SizedBox(
+                          width: constraints.maxWidth * 0.36,
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Image.network(
+                              imageUrl,
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 6),
-                          child: RichText(
-                            text: TextSpan(
-                              text: "\$${raisedAmount.toInt()}",
-                              style: GoogleFonts.urbanist(
-                                color: primaryColor,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w700,
+                        GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            margin: const EdgeInsets.only(right: 6, top: 6),
+                            padding: const EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: primaryColor,
+                              border: Border.all(
+                                width: 0.8,
+                                color: borderColor,
                               ),
-                              children: <TextSpan>[
-                                TextSpan(
-                                  text: ' fund raised from ',
-                                  style: GoogleFonts.urbanist(
-                                    color: Colors.black.withOpacity(0.5),
-                                    fontWeight: FontWeight.w600,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: const Icon(
+                              Icons.bookmark_outline,
+                              size: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: constraints.maxWidth * 0.62,
+                      padding:
+                          const EdgeInsets.only(top: 14, left: 8, bottom: 14),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            title,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.urbanist(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 6),
+                            child: RichText(
+                              text: TextSpan(
+                                text: "\$${raisedAmount.toInt()}",
+                                style: GoogleFonts.urbanist(
+                                  color: primaryColor,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: ' fund raised from ',
+                                    style: GoogleFonts.urbanist(
+                                      color: Colors.black.withOpacity(0.5),
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: "\$${goal.toInt()}",
+                                    style: GoogleFonts.urbanist(
+                                      color: primaryColor,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 4),
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(12),
+                              ),
+                              child: LinearProgressIndicator(
+                                value: (raisedAmount / goal),
+                                color: primaryColor,
+                                backgroundColor: progressBackgroundColor,
+                                minHeight: 5.2,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                RichText(
+                                  text: TextSpan(
+                                    text: "$donatorsCount",
+                                    style: GoogleFonts.urbanist(
+                                      color: primaryColor,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                    children: <TextSpan>[
+                                      TextSpan(
+                                        text: ' Donators',
+                                        style: GoogleFonts.urbanist(
+                                          color: Colors.black.withOpacity(0.5),
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
-                                TextSpan(
-                                  text: "\$${goal.toInt()}",
-                                  style: GoogleFonts.urbanist(
-                                    color: primaryColor,
-                                    fontWeight: FontWeight.w700,
+                                RichText(
+                                  text: TextSpan(
+                                    text: "$daysLeft",
+                                    style: GoogleFonts.urbanist(
+                                      color: primaryColor,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                    children: <TextSpan>[
+                                      TextSpan(
+                                        text: ' days left',
+                                        style: GoogleFonts.urbanist(
+                                          color: Colors.black.withOpacity(0.5),
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
                               ],
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 4),
-                          child: ClipRRect(
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(12),
-                            ),
-                            child: LinearProgressIndicator(
-                              value: (raisedAmount / goal),
-                              color: primaryColor,
-                              backgroundColor: progressBackgroundColor,
-                              minHeight: 5.2,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              RichText(
-                                text: TextSpan(
-                                  text: "$donatorsCount",
-                                  style: GoogleFonts.urbanist(
-                                    color: primaryColor,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                  children: <TextSpan>[
-                                    TextSpan(
-                                      text: ' Donators',
-                                      style: GoogleFonts.urbanist(
-                                        color: Colors.black.withOpacity(0.5),
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              RichText(
-                                text: TextSpan(
-                                  text: "$daysLeft",
-                                  style: GoogleFonts.urbanist(
-                                    color: primaryColor,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                  children: <TextSpan>[
-                                    TextSpan(
-                                      text: ' days left',
-                                      style: GoogleFonts.urbanist(
-                                        color: Colors.black.withOpacity(0.5),
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
