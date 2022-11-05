8f0685a7628e39255b9f10d1163a49de74dd86c1
Hashan Bhanuka <80708655+BhanukaAGH@users.noreply.github.com>
2022-11-05 17:13:01 +0530

    Update firestore_methods.dart

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -10,54 +10,6 @@ class FirestoreMethods {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   var _Dataman = [];
 
-  //! Create Fundraise
-  Future<String> createFundraise({
-    required String uid,
-    required String title,
-    required String category,
-    required double goal,
-    required DateTime expireDate,
-    required String story,
-    required List<Uint8List?> images,
-    required String recipientName,
-    required String recipientPhone,
-    required String recipientEmail,
-    required bool isDraft,
-  }) async {
-    String res = 'some error occured';
-    try {
-      List<String> imageUrls = await StorageMethods()
-          .uploadImageToStorage('fundraises', images, true);
-
-      String fundraiseId = const Uuid().v1();
-
-      Fundraise fundraise = Fundraise(
-        fundraiseId: fundraiseId,
-        uid: uid,
-        title: title,
-        category: category,
-        goal: goal,
-        expireDate: expireDate,
-        publishDate: DateTime.now(),
-        story: story,
-        images: imageUrls,
-        recipientName: recipientName,
-        recipientPhone: recipientPhone,
-        recipientEmail: recipientEmail,
-        isDraft: isDraft,
-      );
-
-      _firestore
-          .collection('fundraisers')
-          .doc(fundraiseId)
-          .set(fundraise.tojson());
-      res = 'success';
-    } catch (err) {
-      res = err.toString();
-    }
-    return res;
-  }
-
   //Save Fundraiser method
   Future<String> savedFundraise({
     required String fundraiseId,
